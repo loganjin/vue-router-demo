c47cfbc419d317099032a79f65840cf6443e35f9
loganjin <loganjin222@gmail.com>
2022-06-14 13:10:56 +0800

    vue-router基础使用

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App'
-// import router from './router'
+// MARK: 当导入目录时，会自动去找index文件，此处等价于'./router/index'
+import router from './router'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  // router,
+  // router: router,
+  router,
   render: h => h(App)
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,22 @@
-// import Vue from 'vue'
-// import Router from 'vue-router'
+import Vue from 'vue'
+// 配置路由相关信息
+import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
 
-// Vue.use(Router)
+// 1.通过Vue.use(插件)，安装插件
+Vue.use(Router)
+
+// 2.创建Router对象
+const routes = [
+
+]
+const router = new Router({
+    // 配置路由和组件之间的应用关系
+    routes
+})
+
+// 3.将router对象挂载（传到）Vue实例中
+export default router
 
 // export default new Router({
 //   routes: [
